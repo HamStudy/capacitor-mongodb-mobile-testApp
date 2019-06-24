62b8fc2e71950424959dcbf855ce61b0b4dafb7a
Ray Bateman <rayb@gradecam.com>
2019-06-24 15:53:19 -0600

    Add some lame tests

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <div v-for="(r, index) of results" :key="index" class="test" :class="{'green': r.status === 'pass', 'red': r.status === 'fail'}">
+      <div class="strong">
+        <span>{{r.name}}... </span><span class="uppercase">{{r.status}}</span>
+      </div>
+      <div v-if="r.text">{{ r.text }}</div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+interface Test {
+  name: string;
+  status: 'running' | 'pass' | 'fail';
+  text?: string;
+}
+
+@Component
+export default class Home extends Vue {
+  results: Test[] = [];
+
+  async runTest(name: string, test: () => Promise<boolean | string>) {
+    let res: Test = { name, status: 'running' };
+    this.results.push(res);
+    try {
+      let testRes = await test();
+      res.status = testRes === true ? 'pass' : 'fail';
+      if (testRes && testRes !== true) {
+        res.text = testRes;
+      }
+    } catch (e) {
+      res.status = 'fail';
+      res.text = e;
+    }
+  }
+
+  async created() {
+    let p = new Promise(() => {});
+    await this.runTest('Wait 5 seconds and pass', async () => {
+      return new Promise((res, rej) => {
+        setTimeout(() => res(true), 5000);
+      });
+    });
+    await this.runTest('Wait 1 seconds and fail', async () => {
+      return new Promise((res, rej) => {
+        setTimeout(() => rej('The system crashed and burned and is still on fire'), 5000);
+      });
+    });
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.red { color: red; }
+.green { color: green; }
+.uppercase { text-transform: uppercase; }
+.strong { font-weight: bold; font-size: 1.3em; }
+</style>
+
